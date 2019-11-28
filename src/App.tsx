3dd1767e3b0342306034ecd5fc576bdef371eee7
Cao Nguyen Van <cao.nv17@gmail.com>
2019-11-28 17:19:42 +0700

    add Dante

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,71 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 
+import Dante2 from 'Dante2'
+
+const ref2 = React.createRef()
+const ref3 = React.createRef()
+window.ref = ref2;
+window.ref2 = ref3;
+
+const initRef = {
+  blocks: [
+    {
+      data:{},
+      depth: 0,
+      entityRanges: [],
+      inlineStyleRanges: [],
+      key: 'aaa',
+      text: 'agbag',
+      type: 'unstyled'
+    }
+  ],
+  entityMap: {}
+}
 const App = () => {
 
+  const content = {
+    blocks: [{
+      data: {},
+      depth: 0,
+      entityRanges: [],
+      inlineStyleRanges: [],
+      key: 'bj1hm',
+      text:'aaaa',
+      type: 'unstyled'
+    }],
+    entityMap: {}
+  }
 
-
+  //@ts-ignore
+  const [value, setValue] = useState( localStorage.getItem('@item') !== null ? JSON.parse(localStorage.getItem('@item')) : null  )
+  // useEffect(()=>{
+  //   console.log('aaa');
+  //   (async ()=>{
+  //     const res = await localStorage.getItem('@item')
+  //     if(res){
+  //       console.log('check', JSON.parse(res))
+  //       setValue(JSON.parse(res))
+  //     }
+      
+  //   })()
+  // }, [])
   return (
-    <div>Testing</div>
+    <div>
+     
+      <Dante2 ref={ref2} content = {value}>
+     
+      </Dante2>
+      <Dante2
+      ref={ref3}
+          onChange
+          ={ (editor:any) => { 
+            window.editor = editor;
+             setValue(editor.emitSerializedOutput()) 
+             localStorage.setItem('@item', JSON.stringify(editor.emitSerializedOutput()))
+            }}
+        />
+    </div>
   );
 
 }
